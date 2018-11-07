6fb9653e10f7e41ae17bc0ce277f488a27662131
mas9612 <mas9612@gmail.com>
2018-11-07 22:00:13 +0900

    add gap feature to VBcode

--- compress/vbcode.go
+++ compress/vbcode.go
@@ -23,6 +23,7 @@ func main() {
 	inFilename := flag.String("in", "", "Input filename")
 	outFilename := flag.String("out", "", "Output filename")
 	decode := flag.Bool("d", false, "Decode. If you omit this flag, -in file will be encoded")
+	gap := flag.Bool("gap", false, "Hold gap between previous number. This flag can use only if numbers are sorted ascending order")
 	flag.Parse()
 
 	if *inFilename == "" || *outFilename == "" {
@@ -31,32 +32,38 @@ func main() {
 	}
 
 	if !*decode {
-		vbEncode(*inFilename, *outFilename)
+		vbEncode(*inFilename, *outFilename, *gap)
 	} else {
-		vbDecode(*inFilename, *outFilename)
+		vbDecode(*inFilename, *outFilename, *gap)
 	}
 }
 
-func vbEncode(inFilename, outFilename string) {
+func vbEncode(inFilename, outFilename string, gap bool) {
 	inFile, err := os.Open(inFilename)
 	if err != nil {
-		log.Fatalln("os.Open: ", err)
+		log.Fatalln(err)
 	}
 	defer inFile.Close()
-	scanner := bufio.NewScanner(inFile)
+	reader := bufio.NewReader(inFile)
 
 	outFile, err := os.Create(outFilename)
 	if err != nil {
-		log.Fatalln("os.Create: ", err)
+		log.Fatalln(err)
 	}
 	defer outFile.Close()
 
 	writer := bufio.NewWriter(outFile)
 	defer writer.Flush()
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		tmp := strings.Split(line, "\t")
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalln(err)
+		}
+		tmp := strings.Split(strings.Trim(line, "\n"), "\t")
 		if len(tmp) < 2 {
 			log.Fatalln("Error: Failed to split with '\t' character")
 		}
@@ -64,33 +71,33 @@ func vbEncode(inFilename, outFilename string) {
 		ids := toIntSlice(idStrings)
 
 		tagBytes := []byte(tag)
-		encoded := encodeNumbers(ids)
+		encoded := encodeNumbers(ids, gap)
 		length := make([]byte, 8)
 		binary.BigEndian.PutUint32(length[0:], uint32(len(tagBytes)))
 		binary.BigEndian.PutUint32(length[4:], uint32(len(encoded)))
 		// write length of bytes before write real data
 		if _, err := writer.Write(length); err != nil {
-			log.Fatalln("Writer.Write: ", err)
+			log.Fatalln(err)
 		}
 		if _, err := writer.Write([]byte(tag)); err != nil {
-			log.Fatalln("Writer.Write: ", err)
+			log.Fatalln(err)
 		}
 		if _, err := writer.Write(encoded); err != nil {
-			log.Fatalln("Writer.Write: ", err)
+			log.Fatalln(err)
 		}
 	}
 }
 
-func vbDecode(inFilename, outFilename string) {
+func vbDecode(inFilename, outFilename string, gap bool) {
 	inFile, err := os.Open(inFilename)
 	if err != nil {
-		log.Fatalln("os.Open: ", err)
+		log.Fatalln(err)
 	}
 	defer inFile.Close()
 
 	outFile, err := os.Create(outFilename)
 	if err != nil {
-		log.Fatalln("os.Create: ", err)
+		log.Fatalln(err)
 	}
 	defer outFile.Close()
 
@@ -104,27 +111,27 @@ func vbDecode(inFilename, outFilename string) {
 			if err == io.EOF {
 				break
 			}
-			log.Fatalln("binary.Read: ", err)
+			log.Fatalln(err)
 		}
 		if err := binary.Read(inFile, binary.BigEndian, &idLength); err != nil {
 			if err == io.EOF {
 				break
 			}
-			log.Fatalln("binary.Read: ", err)
+			log.Fatalln(err)
 		}
 
 		tagBytes := make([]byte, tagLength)
 		idBytes := make([]byte, idLength)
 		if _, err := inFile.Read(tagBytes); err != nil {
-			log.Fatalln("File.Read: ", err)
+			log.Fatalln(err)
 		}
 		if _, err := inFile.Read(idBytes); err != nil {
-			log.Fatalln("File.Read: ", err)
+			log.Fatalln(err)
 		}
-		decoded := decode(idBytes)
+		decoded := decode(idBytes, gap)
 
 		if _, err := fmt.Fprintf(writer, "%s\t%s\n", string(tagBytes), strings.Join(toStringSlice(decoded), ",")); err != nil {
-			log.Fatalln("fmt.Fprintf: ", err)
+			log.Fatalln(err)
 		}
 	}
 }
@@ -144,7 +151,15 @@ func encodeNumber(num int) []byte {
 	return bytes
 }
 
-func encodeNumbers(nums []int) []byte {
+func encodeNumbers(nums []int, gap bool) []byte {
+	if gap {
+		tmp := make([]int, len(nums))
+		tmp[0] = nums[0]
+		for i := 1; i < len(nums); i++ {
+			tmp[i] = nums[i] - nums[i-1]
+		}
+		nums = tmp
+	}
 	byteStream := []byte{}
 	for _, num := range nums {
 		b := encodeNumber(num)
@@ -153,7 +168,7 @@ func encodeNumbers(nums []int) []byte {
 	return byteStream
 }
 
-func decode(bytes []byte) []int {
+func decode(bytes []byte, gap bool) []int {
 	numbers := []int{}
 	var num int
 	for _, b := range bytes {
@@ -166,6 +181,11 @@ func decode(bytes []byte) []int {
 			num = 0
 		}
 	}
+	if gap {
+		for i := 1; i < len(numbers); i++ {
+			numbers[i] = numbers[i] + numbers[i-1]
+		}
+	}
 	return numbers
 }
 
@@ -173,7 +193,7 @@ func toIntSlice(strs []string) []int {
 	nums := make([]int, len(strs))
 	for i, str := range strs {
 		if s, err := strconv.Atoi(str); err != nil {
-			log.Fatalln("strconv.Atoi: ", err)
+			log.Fatalln(err)
 		} else {
 			nums[i] = s
 		}
